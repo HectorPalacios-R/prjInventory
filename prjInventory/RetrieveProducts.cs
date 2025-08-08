e63b8a3494f23660fc1b82422efcb109ec2e559b
HectorPalacios-R <ST10302442@vcconnect.edu.za>
2025-08-08 14:14:51 +0200

    Add project files.

---
+++ prjInventory/RetrieveProducts.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prjInventoryManagement
+{
+    public static class RetrieveProducts
+    {
+        public static double GetTotalPrice(List<Product> products)
+        {
+            // Calculate the total price of all products  
+            return products.Sum(p => p.Price);
+        }
+        public static List<Product> FilterProductsByStock(this List<Product> products)
+        {
+            // Filter products that are in stock (Quantity >
+            return products.OrderBy(p => p.Quantity).ToList();
+        }
+        public static List<Product> SelectProducts(this List<Product> products)
+        {
+            // Filter products that are in stock (Quantity >
+            return (List<Product>)products.Select(p => new { p.Name, p.Price });
+        }
+    }
+}
